1a76b6185f8f2295e7f72d019484b756cd5a3ea7
acheird <acheird@gmail.com>
2025-01-10 11:12:40 +0200

    create styles

---
+++ client/src/components/Form/Form.jsx
@@ -0,0 +1,8 @@
+import useStyles from "./styles";
+
+const Form = () => {
+  const classes = useStyles();
+  return <h1>FORM</h1>;
+};
+
+export default Form;
---
+++ client/src/components/Posts/Posts.jsx
@@ -0,0 +1,13 @@
+import Post from "./Post/Post";
+import useStyles from "./styles";
+const Posts = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <h1>POSTS</h1>;
+      <Post />
+      <Post />
+    </>
+  );
+};
+export default Posts;
