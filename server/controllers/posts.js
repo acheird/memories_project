fb8c41911460d176679415c3eef2218fd43cd714
acheird <acheird@gmail.com>
2025-01-23 14:27:10 +0200

    fix deprecated function

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -45,12 +45,12 @@ export const updatePost = async (req, res) => {
 };
 
 export const deletePost = async (req, res) => {
-  const { _id } = req.params;
+  const { id } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(_id))
+  if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send("No post with that id");
 
-  await PostMessage.findByIdAndRemove(id);
+  await PostMessage.findByIdAndDelete(id);
 
   res.json({ message: "Post deleted successfully" });
 };
